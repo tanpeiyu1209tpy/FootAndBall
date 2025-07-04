ec61ba5995ee2c4047e2e317c997beae5345ef78
Tan Pei Yu <120084583+tanpeiyu1209tpy@users.noreply.github.com>
2025-07-04 14:03:35 +0800

    Update data_reader.py

--- data/data_reader.py
+++ data/data_reader.py
@@ -7,7 +7,7 @@ import torch
 from torch.utils.data import Sampler, DataLoader, ConcatDataset
 
 from data.issia_dataset import create_issia_dataset, IssiaDataset
-from data.spd_bmvc2017_dataset import create_spd_dataset
+#from data.spd_bmvc2017_dataset import create_spd_dataset
 from misc.config import Params
 
 
@@ -23,17 +23,18 @@ def make_dataloaders(params: Params):
             val_issia_dataset = create_issia_dataset(params.issia_path, params.issia_val_cameras, mode='val',
                                                      only_ball_frames=True)
             
-    if params.spd_set is None:
-        train_spd_dataset = None
-    else:
-        train_spd_dataset = create_spd_dataset(params.spd_path, params.spd_set, mode='train')
+    #if params.spd_set is None:
+    #    train_spd_dataset = None
+    #else:
+    #    train_spd_dataset = create_spd_dataset(params.spd_path, params.spd_set, mode='train')
 
     dataloaders = {}
     if val_issia_dataset is not None:
         dataloaders['val'] = DataLoader(val_issia_dataset, batch_size=2, num_workers=params.num_workers,
                                         pin_memory=True, collate_fn=my_collate)
     
-    train_dataset = ConcatDataset([train_issia_dataset, train_spd_dataset])
+    #train_dataset = ConcatDataset([train_issia_dataset, train_spd_dataset])
+    train_dataset = train_issia_dataset
     batch_sampler = BalancedSampler(train_dataset)
     dataloaders['train'] = DataLoader(train_dataset, sampler=batch_sampler, batch_size=params.batch_size,
                                       num_workers=params.num_workers, pin_memory=True, collate_fn=my_collate)
@@ -55,7 +56,7 @@ class BalancedSampler(Sampler):
         self.data_source = data_source
         self.sample_ndx = []
         self.generate_samples()
-
+    '''
     def generate_samples(self):
         # Sample generation function expects concatenation of 2 datasets: one is ISSIA CNR and the other is SPD
         # or only one ISSIA CNR dataset.
@@ -92,6 +93,19 @@ class BalancedSampler(Sampler):
             spd_samples_ndx = []
 
         self.sample_ndx = issia_samples_ndx + spd_samples_ndx
+        random.shuffle(self.sample_ndx)
+    '''
+    def generate_samples(self):
+        # Sample generation function for ISSIA CNR dataset only
+        assert isinstance(self.data_source, IssiaDataset), 'Training data must be ISSIA CNR dataset.'
+        
+        issia_ds = self.data_source
+        n_ball_images = len(issia_ds.ball_images_ndx)
+        # no_ball_images = 0.5 * ball_images
+        n_no_ball_images = min(len(issia_ds.no_ball_images_ndx), int(0.5 * n_ball_images))
+        
+        self.sample_ndx = list(issia_ds.ball_images_ndx) + random.sample(list(issia_ds.no_ball_images_ndx),
+                                                                         n_no_ball_images)
         random.shuffle(self.sample_ndx)
 
     def __iter__(self):
